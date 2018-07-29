f97f3db1bce8a217d479a286efe6f0d66db4003a
Yaroslav Savaryn <yariksav@gmail.com>
2018-07-29 14:38:16 +0300

    make recaptcha not necessary when use only test fakes

--- src/Captcha/index.js
+++ src/Captcha/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const Recaptcha2 = require('recaptcha2')
 const Env = use('Env')
 const GE = require('@adonisjs/generic-exceptions')
 
@@ -8,12 +7,7 @@ class Captcha {
 
   constructor (Config) {
     this._fake = false
-    const siteKey = Config.get('services.recaptcha.siteKey') || Env.get('RECAPTCHA_SITE_KEY')
-    const secretKey = Config.get('services.recaptcha.secretKey') || Env.get('RECAPTCHA_SECRET')
-    this.recaptcha = new Recaptcha2 ({
-      siteKey,
-      secretKey
-    })
+    this._recaptcha = null
   }
 
   fake () {
@@ -24,15 +18,30 @@ class Captcha {
     this._fake = false
   }
 
+  getRecaptchaInstance () {
+    if (!this._recaptcha) {
+      const Recaptcha2 = require('recaptcha2')
+      const siteKey = Config.get('services.recaptcha.siteKey') || Env.get('RECAPTCHA_SITE_KEY')
+      const secretKey = Config.get('services.recaptcha.secretKey') || Env.get('RECAPTCHA_SECRET')
+      this._recaptcha = new Recaptcha2 ({
+        siteKey,
+        secretKey
+      })
+    }
+    return this._recaptcha
+  }
+
   async validate (token) {
     if (this._fake) {
       return true
     }
+
     if (!token) {
       throw new GE.HttpException('Recaptcha token not found', 401, 'E_INVALID_CAPTCHA')
     }
+
     try {
-      await this.recaptcha.validate(token)
+      await this.getRecaptchaInstance().validate(token)
     } catch (e) {
       throw new GE.HttpException('Invalid captcha', 401, 'E_INVALID_CAPTCHA')
     }
